b08d30822f2432da2aadc877f37000fb30bae45a
Volodymyr Pankiv <ivovan4ik@gmail.com>
2024-11-30 16:56:23 +0200

    add forms to adminpanel in admin.py

--- kitchen/admin.py
+++ kitchen/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
 from .models import Dish, DishType, Cook
+from .forms import DishForm, DishTypeForm, CookForm
 
-admin.site.register(Dish)
-admin.site.register(DishType)
-admin.site.register(Cook)
+
+class DishAdmin(admin.ModelAdmin):
+    form = DishForm
+    list_display = ["name", "dish_type", "price"]
+    search_fields = ["name"]
+    list_filter = ["dish_type"]
+
+
+class DishTypeAdmin(admin.ModelAdmin):
+    form = DishTypeForm
+    list_display = ["name"]
+    search_fields = ["name"]
+
+
+class CookAdmin(admin.ModelAdmin):
+    form = CookForm
+    list_display = ["first_name", "last_name", "years_of_experience"]
+    search_fields = ["first_name", "last_name"]
+
+
+admin.site.register(Dish, DishAdmin)
+admin.site.register(DishType, DishTypeAdmin)
+admin.site.register(Cook, CookAdmin)
