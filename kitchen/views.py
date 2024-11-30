22b5d93db5ee76b7810647aaf409e7b5644b0735
Volodymyr Pankiv <ivovan4ik@gmail.com>
2024-11-30 17:41:24 +0200

    add class bases views in views.py

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import (
+    CreateView,
+    UpdateView,
+    ListView,
+    DetailView,
+    DeleteView,
+)
+from .models import Dish
+from .forms import DishForm
 
-# Create your views here.
+
+class DishCreateView(CreateView):
+    model = Dish
+    form_class = DishForm
+    template_name = "dish_create.html"
+    success_url = reverse_lazy("dish_list")
+
+
+class DishListView(ListView):
+    model = Dish
+    template_name = "dish_list.html"
+    context_object_name = "dishes"
+
+
+class DishDetailView(DetailView):
+    model = Dish
+    template_name = "dish_detail.html"
+    context_object_name = "dish"
+
+
+class DishUpdateView(UpdateView):
+    model = Dish
+    form_class = DishForm
+    template_name = "dish_update.html"
+    success_url = reverse_lazy("dish_list")
+
+
+class DishDeleteView(DeleteView):
+    model = Dish
+    template_name = "dish_confirm_delete.html"
+    success_url = reverse_lazy("dish_list")
