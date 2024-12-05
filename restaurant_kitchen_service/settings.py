2ac3d218e67d30858ca0c01252a33996f0902311
Volodymyr Pankiv <ivovan4ik@gmail.com>
2024-12-05 19:16:36 +0200

    update settings

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-a&*^$75@+^_gl1g%qq%!)lr_jgi34p^_nt_v=-=kr-vzxmcg5t"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-a&*^$75@+^_gl1g%qq%!)lr_jgi34p^_nt_v=-=kr-vzxmcg5t")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
